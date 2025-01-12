8352c3b7dfc56e2e5191f4032acfe6c73769986e
uzair <uzairnaeem303@gmail.com>
2025-01-12 15:58:37 +0500

    added cli command to import records

--- fuel_router_app/management/commands/import_stations.py
+++ fuel_router_app/management/commands/import_stations.py
@@ -1,38 +1,85 @@
-from django.core.management.base import BaseCommand
 import csv
-from fuel_router_app.models import FuelStation
-from fuel_router_app.route_optimizer import RouteOptimizer
+import requests
+from itertools import islice
 from django.conf import settings
-from pathlib import Path
-from fuel_router_app.management.commands.utils import geocode_truckstops
+from django.core.management.base import BaseCommand
+from fuel_router_app.models import FuelStation
 
 
 class Command(BaseCommand):
-    help = 'Import fuel stations from CSV file'
+    help = 'Import fuel stations from CSV file in batches'
 
     def handle(self, *args, **options):
         base_dir = settings.BASE_DIR
-        fule_prices_csv = base_dir / 'fuel-prices-for-be-assessment.csv'
-        route_optimizer = RouteOptimizer()
+        fuel_prices_csv = base_dir / 'fuel-prices-for-be-assessment.csv'
 
-        with open(fule_prices_csv, mode='r') as file:
+        with open(fuel_prices_csv, mode='r') as file:
             reader = csv.DictReader(file)
-            truckstops = list(reader)
-        
-        geocoded_truckstops = geocode_truckstops(truckstops, route_optimizer)
-        for stop in geocoded_truckstops:
-            try:
+            start = 0
+            limit = 100
+            while True:
+                print(f"processing rows from: {start*limit}")
+                batch = list(islice(reader, limit))  # Read 100 rows at a time
+                if not batch:
+                    break
+                
+                self.process_batch(batch)
+                start+=1
+
+    def process_batch(self, batch):
+        for stop in batch:
+            if FuelStation.objects.filter(opis_id=stop["OPIS Truckstop ID"]).exists():
+                print("Id already exists")
+                continue
+            
+            print(f"process shop : {stop['OPIS Truckstop ID']}")
+            formatted_location = stop['Address'].replace("EXIT", "").replace("&", "and").replace("  ", " ").strip()
+            lat, lon = None, None
+
+            for attempt in [
+                f"{formatted_location}, {stop['City']}, {stop['State']}, USA",
+                f"{stop['Address'].replace('EXIT', '').strip()}, {stop['City']}, {stop['State']}, USA",
+                f"{stop['City']}, {stop['State']}, USA"
+            ]:
+                lat, lon = self.geocode_location(attempt)
+                if lat and lon:
+                    break  # Exit once valid coordinates are found
+
+            if lat is not None and lon is not None:
                 FuelStation.objects.create(
-                    opis_id=stop['Opis id'],
+                    opis_id=stop['OPIS Truckstop ID'],
                     name=stop['Truckstop Name'],
                     address=stop['Address'],
                     city=stop['City'],
                     state=stop['State'],
-                    rack_id=stop['Rack id'],
-                    lat=stop['Latitude'],
-                    lon=stop['Longitude'],
+                    rack_id=stop['Rack ID'],
+                    lat=lat,
+                    lon=lon,
                     retail_price=stop['Retail Price'],
                 )
-                print(f"Successfully imported {stop['Truckstop Name']}")
-            except Exception as e:
-                print(f"Failed to import {stop['Truckstop Name']}: {e}")
+                
+            else:
+                print(f"Failed to geocode location: {formatted_location}")
+
+        print(f"Imported fuel stations successfully.")
+
+    def geocode_location(self, location):
+        url = 'https://nominatim.openstreetmap.org/search'
+        params = {
+            'q': location,
+            'format': 'json',
+            'limit': 10,
+            'countrycodes': 'us'
+        }
+        headers = {'User-Agent': 'RouteOptimizer/1.0'}
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            if data:
+                for result in data:
+                    if 'lat' in result and 'lon' in result:
+                        return float(result['lat']), float(result['lon'])
+        except requests.RequestException as e:
+            print(f"Geocoding request failed: {e}")
+        return None, None
